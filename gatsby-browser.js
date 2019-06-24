ed5f9f83ece71bcc0af8d058bb35c4b25f98ba9e
Kahvi Patel <iamkahvi@gmail.com>
2019-06-23 22:10:18 -0700

    adding book list to homepage

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,3 @@
 // functional CSS
-import "./src/styles/tachyons.css"
-import "./src/styles/links.scss"
+import './src/styles/tachyons.css';
+import './src/styles/links.scss';
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,58 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { Link, graphql } from 'gatsby';
+import styled from 'tachyons-components';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import BookListLogo from '../../static/booklist2.png';
 
 function BlogIndex(props) {
   const { data, location } = props;
   const { title, description } = data.site.siteMetadata;
   const posts = data.allMarkdownRemark.edges;
 
+  const YearHead = styled('h1')`
+    roboto f4 fw4 tc faded-blue mv4
+  `;
+
+  const PostTitle = styled('h3')`
+    mt0 mb2 pt3 bt b--light-gray
+  `;
+
+  const PostDescription = styled('p')`
+    f6 roboto faded-blue
+  `;
+
   return (
     <Layout location={location} title={title} description={description}>
       <SEO title={title} />
 
+      <Link className="booklist alink mt2 mb4" to="/book-list">
+        <img src={BookListLogo} alt="Book List Logo" />
+      </Link>
+
       {posts.map((edge) => {
         const { node, previous } = edge;
         const postTitle = node.frontmatter.title || node.fields.slug;
-        const previousPost = previous ? previous : node;
+        const previousPost = previous || node;
         const currYear = node.frontmatter.date.split(' ').pop();
         const previousYear = previousPost.frontmatter.date.split(' ').pop();
         const yearHeader = currYear !== previousYear;
+        const postDate = node.frontmatter.date.split(',')[0];
 
         return (
           <div key={node.fields.slug}>
-            {yearHeader && <h1 className="roboto f4 fw4 tc faded-blue mb4">{currYear}</h1>}
-            <h3 className="mt0 mb2 pt3 bt b--light-gray">
-              <Link style={{ boxShadow: 'none' }} className="alink roboto b faded-orange" to={node.fields.slug}>
+            {yearHeader && <YearHead>{currYear}</YearHead>}
+            <PostTitle>
+              <Link className="alink roboto b faded-orange" to={node.fields.slug}>
                 {postTitle}
               </Link>
-            </h3>
-            <small className="f5 roboto faded-blue fr">{node.frontmatter.date.split(',')[0]}</small>
-            <p
+            </PostTitle>
+            <small className="f5 roboto faded-blue fr">{postDate}</small>
+            <PostDescription
               dangerouslySetInnerHTML={{
                 __html: node.frontmatter.description || node.excerpt,
               }}
-              className="f6 roboto faded-blue"
             />
           </div>
         );
