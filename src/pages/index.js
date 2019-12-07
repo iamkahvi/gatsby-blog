9c02f9f408c30ad92b8347ac6bc288c46e5b82f8
Kahvi Patel <iamkahvi@gmail.com>
2019-12-07 14:10:33 -0800

    KP: Adding search function to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,62 +1,76 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link, graphql } from "gatsby"
 
 // import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-class BlogIndex extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-    const posts = data.allMarkdownRemark.edges
-    const description = data.site.siteMetadata.description
+const BlogIndex = ({ data, location }) => {
+  const [search, setSearch] = useState("")
 
-    return (
-      <Layout
-        location={this.props.location}
-        title={siteTitle}
-        description={description}
-      >
-        <SEO title={siteTitle} />
+  const siteTitle = data.site.siteMetadata.title
+  const posts = data.allMarkdownRemark.edges
+  const description = data.site.siteMetadata.description
 
-        {posts.map(edge => {
+  return (
+    <Layout location={location} title={siteTitle} description={description}>
+      <input
+        onChange={e => setSearch(e.target.value)}
+        placeholder="search..."
+        value={search}
+        default=""
+        id="home"
+      />
+      <SEO title={siteTitle} />
+
+      {posts
+        .filter(
+          edge =>
+            edge.node.frontmatter.title
+              .toLowerCase()
+              .includes(search.toLowerCase()) || search === ""
+        )
+        .map((edge, i) => {
           const { node, previous } = edge
           const title = node.frontmatter.title || node.fields.slug
           const previousPost = previous ? previous : node
           const currYear = node.frontmatter.date.split(" ").pop()
           const previousYear = previousPost.frontmatter.date.split(" ").pop()
-          const yearHeader = currYear !== previousYear ? true : false
+          const yearHeader = i == 0 || currYear !== previousYear ? true : false
 
           return (
-            <div className="pv3 bt b--light-gray" key={node.fields.slug}>
+            <>
               {yearHeader && (
                 <h1 className="roboto f4 fw4 tc faded-blue mb4">{currYear}</h1>
               )}
-              <h3 className="mt0 mb2 ">
-                <Link
-                  style={{ boxShadow: `none` }}
-                  className="alink roboto b faded-orange"
-                  to={node.fields.slug}
-                >
-                  {title}
-                </Link>
-              </h3>
-              <small className="f5 roboto faded-blue fr mb3">
-                {node.frontmatter.date.split(",")[0]}
-              </small>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-                className="f6 roboto faded-blue"
-              />
-            </div>
+              <div
+                className="pv3 bt b--light-gray flex items-center justify-between"
+                key={node.fields.slug}
+              >
+                <h3 className="mv0">
+                  <Link
+                    style={{ boxShadow: `none` }}
+                    className="alink roboto b faded-orange"
+                    to={node.fields.slug}
+                  >
+                    {title}
+                  </Link>
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: node.frontmatter.description || node.excerpt,
+                    }}
+                    className="f6 fw4 roboto faded-blue"
+                  />
+                </h3>
+                <small className="f5 roboto faded-blue fr">
+                  {node.frontmatter.date.split(",")[0]}
+                </small>
+              </div>
+            </>
           )
         })}
-      </Layout>
-    )
-  }
+    </Layout>
+  )
 }
 
 export default BlogIndex
