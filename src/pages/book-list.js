e93b2a78e2d491d5b2d13df8664a7a56619545c1
Kahvi Patel <iamkahvi@gmail.com>
2020-03-12 23:40:10 -0700

    adding fetch button for testing

---
+++ src/pages/book-list.js
@@ -0,0 +1,59 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from "react"
+import { graphql } from "gatsby"
+import styled from "styled-components"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+function BookList(props) {
+  const { data, location } = props
+  const { html, title } = data.allGhostPage.nodes[0]
+
+  const Body = styled.div`
+    font-family: "Roboto";
+    margin-bottom: 3rem;
+    line-height: 1.5;
+    color: #297373;
+  `
+
+  const addEmail = async () => {
+    const res = await fetch("http://localhost:3000/email", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        email: "iamkahvi@gmail.com",
+      }),
+    })
+    const data = await res.json()
+    console.log(data)
+  }
+
+  return (
+    <Layout
+      location={location}
+      title="book list"
+      description="Somewhere for my books"
+    >
+      <SEO title="book list" />
+      <button onClick={addEmail}>addEmail</button>
+      <h1 className="mt0">{title}</h1>
+      <Body className="textBody" dangerouslySetInnerHTML={{ __html: html }} />
+    </Layout>
+  )
+}
+
+export default BookList
+
+export const pageQuery = graphql`
+  query {
+    allGhostPage(filter: { title: { eq: "My Book List" } }) {
+      nodes {
+        html
+        title
+      }
+    }
+  }
+`
