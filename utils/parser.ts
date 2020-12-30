d7e4dceca536c0192497378fb883b7586d6e046f
Kahvi Patel <iamkahvi@gmail.com>
2020-12-30 18:39:56 -0500

    KP: parser works, adding contentful management function

--- utils/parser.ts
+++ utils/parser.ts
@@ -1,7 +1,10 @@
 import { text } from "./data.js";
+const contentful = require("contentful-management");
 
 interface BookListItem {
   title: string;
+  author: string;
+  date: string;
   description: string;
 }
 
@@ -11,24 +14,39 @@ export function parser(text: string): BookList {
   let bList: BookList = [];
 
   const lines = text.split("\n").filter(Boolean);
-  // console.log(lines);
 
-  const titleRegex = /^\ \*\ (.*)$/m;
+  const titleLineRegex = /\*\ (.*)\ by\ (.*)/m;
+  const dateReg = /(\d{2})\/(\d{2})\/(\d{2,4})/m;
   const descRegex = /^\ {3}(.*)$/m;
 
-  let i = 0;
-  while (i < lines.length) {
-    const m = lines[i].match(titleRegex);
-    if (m !== null) {
+  for (let i = 0; i < lines.length; ) {
+    const matches = lines[i].match(titleLineRegex);
+    if (matches !== null && matches.length === 3) {
+      const [_, title, authorDate] = matches;
       let book: BookListItem = {
-        title: m[1],
+        title,
+        author: "",
+        date: "",
         description: "",
       };
+
+      const dateMatches = authorDate.match(dateReg);
+      if (dateMatches !== null && dateMatches.length === 4) {
+        const [_, day, month, year] = dateMatches;
+        book.author = authorDate
+          .split("-")
+          .slice(0, -1)
+          .join("-")
+          .trim();
+        book.date = `${year.length === 2 ? "20" + year : year}-${month}-${day}`;
+      } else {
+        book.author = authorDate.trim();
+      }
 
       const start = ++i;
 
       // Get to end of description
-      while (!titleRegex.test(lines[i]) && i < lines.length) {
+      while (!titleLineRegex.test(lines[i]) && i < lines.length) {
         const m = lines[i].match(descRegex);
         if (m !== null) {
           lines[i] = m[1];
@@ -44,3 +62,52 @@ export function parser(text: string): BookList {
   }
   return bList;
 }
+
+export async function addEntries(bl: BookList) {
+  const contentDeliveryAPI = "tMNTyF_06H4c98VNebh_yox4fCSwsM7nZ7NumKA0b_4";
+  const client = contentful.createClient({
+    accessToken: "CFPAT-_T81OchPsVv4bm2wCsVScOzsIAfPEgYQ1dntNcIT5uI",
+  });
+
+  // Create entry
+  const space = await client.getSpace("rbthbhshshw9");
+  const env = await space.getEnvironment("master");
+  const entry = await env.createEntry("bookListItem", {
+    fields: {
+      bookTitle: {
+        "en-US": "Entry title 3",
+      },
+      bookAuthor: {
+        "en-US": "Someone",
+      },
+      dateFinished: {
+        "en-US": "2020-12-11",
+      },
+      bookDescription: {
+        "en-US": {
+          nodeType: "document",
+          data: {},
+          content: [
+            {
+              nodeType: "paragraph",
+              content: [
+                {
+                  nodeType: "text",
+                  marks: [],
+                  value: "I am an odd paragraph.",
+                  data: {},
+                },
+              ],
+              data: {},
+            },
+          ],
+        },
+      },
+    },
+  });
+
+  console.log(entry);
+}
+
+const bl = parser(text);
+console.log(JSON.stringify(bl));
