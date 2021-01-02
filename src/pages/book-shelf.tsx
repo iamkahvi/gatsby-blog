ac9cad3855b5d4fe508d9c16d32065c4f56180ca
Kahvi Patel <iamkahvi@gmail.com>
2021-01-01 22:36:12 -0500

    KP: adding support for anchors in book shelf

--- src/pages/book-shelf.tsx
+++ src/pages/book-shelf.tsx
@@ -4,15 +4,31 @@ import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import SearchBar from "../components/searchBar";
 import { BookShelfProps, BookEdge } from "../types/types";
 
 const yearKey = {
   2020: "this year",
   2019: "2019",
-  2018: "probably in 2018",
+  2018: "2018",
   2017: "possibly in 2017",
-  2016: "before 2017",
+  2016: "probably before 2017",
 };
+
+const anchorIcon = (
+  <svg
+    aria-hidden="true"
+    height="16"
+    version="1.1"
+    viewBox="0 0 16 16"
+    width="16"
+  >
+    <path
+      fill-rule="evenodd"
+      d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
+    ></path>
+  </svg>
+);
 
 function BookList(props: BookShelfProps) {
   const [search, setSearch] = useState("");
@@ -27,14 +43,21 @@ function BookList(props: BookShelfProps) {
 
   const renderBook = ({ node, previous }: BookEdge) => {
     const prevYear = previous ? previous.year : null;
+    const idLink = node.bookTitle
+      .replace(/[!'’.()*]/g, "")
+      .replace(/\s+/g, "-")
+      .toLowerCase();
     return (
       <>
         {prevYear !== node.year && (
           <h2 className="underline">{yearKey[node.year]}</h2>
         )}
-        <li>
-          <strong>{node.bookTitle}</strong> by {node.bookAuthor}
-          {parseInt(node.year) > 2018 && <em> - {node.dateFinished}</em>}
+        <li id={idLink} className="book mb4">
+          {/* <a className="book anchor faded-blue b" href={`#${idLink}`}>
+            {anchorIcon}
+          </a> */}
+          {node.bookTitle} by {node.bookAuthor}
+          {parseInt(node.year) > 2018 && <em> - {node.dateFinished} </em>}
           <br></br>
           <div
             dangerouslySetInnerHTML={{
@@ -62,12 +85,11 @@ function BookList(props: BookShelfProps) {
             __html: documentToHtmlString(JSON.parse(intro.raw)),
           }}
         />
-        <input
-          onChange={handleSearch}
-          placeholder="search..."
-          value={search}
-          data-default=""
-          className="roboto mb4 f3 normal"
+        <SearchBar
+          handleSearch={handleSearch}
+          placeholderText="search books..."
+          searchVal={search}
+          isSticky={false}
         />
         {books
           .filter(
