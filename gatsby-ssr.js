d7a09feb85b044388b000cc7f9c02d51f9ef2c09
Kahvi Patel <kahvi.patel@shopify.com>
2022-02-05 16:53:11 -0800

    Cleaning up

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,40 +4,38 @@ import React from "react";
 const MagicScriptTag = () => {
   const codeToRunOnClient = `
     (function () {
-    const themeMap = ${JSON.stringify(themeMap)};
-    function getInitialColorMode() {
+      const themeMap = ${JSON.stringify(themeMap)};
+      function getInitialColorMode() {
         const persistedColorPreference = window.localStorage.getItem("color-theme");
         const hasPersistedPreference = typeof persistedColorPreference === "string";
-        // If the user has explicitly chosen light or dark,
-        // let's use it. Otherwise, this value will be null.
+
         if (hasPersistedPreference) {
-        return persistedColorPreference;
+          return persistedColorPreference;
         }
-        // If they haven't been explicit, let's check the media
-        // query
+
         const mql = window.matchMedia("(prefers-color-scheme: dark)");
         const hasMediaQueryPreference = typeof mql.matches === "boolean";
+
         if (hasMediaQueryPreference && mql.matches) {
-            return "dark";
+          return "dark";
         }
-        // If they are using a browser/OS that doesn't support
-        // color themes, let's default to 'light'.
+
         return "classic";
-    }
-    function setRootStyles(theme) {
+      }
+      function setRootStyles(theme) {
         const root = document.documentElement;
         Object.entries(themeMap[theme]).forEach((val) => {
-        const [k, v] = val;
-        root.style.setProperty(k, v);
+          const [k, v] = val;
+          root.style.setProperty(k, v);
 
-        // Ugly fix for selection color
-        if (k == "--c-main") {
+          // Ugly fix for selection color
+          if (k == "--c-main") {
             root.style.setProperty("--c-main-selection", v + "50");
-        }
+          }
         });
-    }
-    const colorMode = getInitialColorMode();
-    setRootStyles(colorMode);
+      }
+      const colorMode = getInitialColorMode();
+      setRootStyles(colorMode);
     })();`;
 
   // eslint-disable-next-line react/no-danger
