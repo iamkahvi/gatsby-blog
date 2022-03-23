2fb74131edef02ac7e6b2008566f8a7488f0754a
Kahvi Patel <kahvi.patel@shopify.com>
2022-03-23 12:19:37 -0700

    Updating absolute path

--- scripts/copy_to_prod.sh
+++ scripts/copy_to_prod.sh
@@ -4,4 +4,4 @@ echo building gatsby
 gatsby build
 
 echo copying to prod
-rsync -azP ../public/ root@142.93.152.73:/var/www/kahvipatel.com/prod-build
+rsync -azP /Users/Kahvi/Developer/gatsby-blog/public/ root@142.93.152.73:/var/www/kahvipatel.com/prod-build
