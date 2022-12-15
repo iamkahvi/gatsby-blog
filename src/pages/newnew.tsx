5424aa9eb029fbba9450f5eff457ff5a5e84a389
Kahvi Patel <kahvi.patel@shopify.com>
2022-12-14 17:19:46 -0800

    Cool cool cool

--- src/pages/newnew.tsx
+++ src/pages/newnew.tsx
@@ -1,11 +1,71 @@
 import React from "react";
+import SEO from "../components/seo";
+import { Link, graphql, navigate } from "gatsby";
+import { IndexProps, PostEdge } from "../types/types";
 
-export default function thatNewNewShit() {
+export default function thatNewNewShit({ data, location }: IndexProps) {
+  console.log(data);
+
   return (
-    <div className="newnew">
-      <h1 id="hello">new blog YEAH BABY</h1>
-      <h2>i'm gonna write all my shit like this from now on</h2>
-      <p>and this the p tag</p>
-    </div>
+    <>
+      <SEO title="newnew" />
+      <div className="newnew">
+        <div className="container">
+          <h1 id="hello">new blog YEAH BABY</h1>
+          <h2>i'm gonna write all my shit like this from now on</h2>
+          {data.allMarkdownRemark.edges.map(({ node }) => {
+            const slug = node.frontmatter.title
+              .replace(/[!'’.()*]/g, "")
+              .replace(/\s+/g, "-")
+              .toLowerCase();
+            const title = node.frontmatter.title || node.fields.slug;
+            
+            return (
+              <p>
+                <a href={slug}>{title}</a>
+              </p>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 }
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+    allMarkdownRemark(
+      filter: {
+        frontmatter: { layout: { eq: "post" }, published: { ne: false } }
+      }
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        previous {
+          frontmatter {
+            year: date(formatString: "YYYY")
+          }
+        }
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            year: date(formatString: "YYYY")
+            displayDate: date(formatString: "MMMM Do")
+            displayDateSmall: date(formatString: "MMM D")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`;
